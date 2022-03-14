48a54554732cd4828a1b8a3f65c979617d4b9204
sidiDev <sididev3@gmail.com>
2022-03-14 18:56:49 +0000

    debbuging

--- pages/confirmation/[token].js
+++ pages/confirmation/[token].js
@@ -3,20 +3,27 @@ import { useRouter } from "next/router"
 import { useEffect } from "react";
 import url from "../../url/url";
 
-const token = () => {
+const token = ({ data }) => {
 
     const router = useRouter()
     const { token } = router.query
 
+    // useEffect( async () => {
+    //     if (token) {
+    //         // const { data } = await axios.get(`${url}/api/confirmation/${token}`, { params: {token} })
+    //         console.log(data);
+    //         if (data && data.exist) {
+    //             router.push(`/dashboard/${data.id}`)
+    //         } else router.push("/signup")
+    //     }
+    // }, [token])
+
     useEffect( async () => {
-        if (token) {
-            const { data } = await axios.get(`${url}/api/confirmation/${token}`, { params: {token} })
-            console.log(data);
-            if (data && data.exist) {
-                router.push(`/dashboard/${data.id}`)
-            } else router.push("/signup")
-        }
-    }, [token])
+            // const { data } = await axios.get(`${url}/api/confirmation/${token}`, { params: {token} })
+        if (data && data.exist) {
+            router.push(`/dashboard/${data.id}`)
+        } else router.push("/signup")
+    }, [])
 
     
     return (
@@ -26,14 +33,14 @@ const token = () => {
     )
 }
 
-// index.getInitialProps = async ({ query }) => {
-//     const { token } = query
-//     // const { data } = await axios.get(`${url}/api/confirm-email/${token}`)
-//     return {
-//         token
-//     }
+token.getInitialProps = async ({ query }) => {
+    const { token } = query
+    const { data } = await axios.get(`${url}/api/confirmation/${token}`, { params: {token} })
+    return {
+        data
+    }
 
-// }
+}
 
 export default token
 
