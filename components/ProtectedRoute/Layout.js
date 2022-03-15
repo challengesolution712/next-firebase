a909521ce942600143b7e898cf7cc33a5e5bfe99
sidiDev <sididev3@gmail.com>
2022-03-15 22:47:52 +0000

    Simple changes

--- components/ProtectedRoute/Layout.js
+++ components/ProtectedRoute/Layout.js
@@ -3,8 +3,6 @@ import Page404 from "../Page404/Page404"
 export default (props) => {
     const { children, data } = props
 
-    console.log(data);
-
     return (
         data.value && data.value.loggedIn ? children : (
             <Page404 />
