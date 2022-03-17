1296d75a62f33fda4f560728fac9fbd092f8b987
sidiDev <sididev3@gmail.com>
2022-03-17 23:19:15 +0000

    Debugging

--- sendMail/sendMail.js
+++ sendMail/sendMail.js
@@ -1,8 +1,8 @@
 import mail from '@sendgrid/mail'
 
-mail.setApiKey(SENDGRID_API_KEY)
-
 export default ({ to, subject, text, html }) => {
+    
+    mail.setApiKey(SENDGRID_API_KEY)
     
     return new Promise((resolve, reject) => {
 
